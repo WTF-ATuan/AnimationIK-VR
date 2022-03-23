809fdd0e7b45566b1cbd92992f0221fe4bd75f4b
ATuan <4A8K0069@stust.edu.tw>
2022-03-23 15:35:51 +0800

    added physics Hand

---
+++ Assets/Actor/Scripts/PhysicsHand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Actor.Scripts{
+	public class PhysicsHand : MonoBehaviour{
+		[SerializeField] private GameObject followObject;
+		[SerializeField] private Vector3 positionOffset;
+		[SerializeField] private float followSpeed = 10f;
+		
+		private new Rigidbody rigidbody;
+		
+		private void Start(){
+			var followPosition = followObject.transform.position;
+			positionOffset = transform.position - followPosition;
+			transform.position = followPosition;
+			rigidbody = GetComponent<Rigidbody>();
+			rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+			rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+			rigidbody.mass = 20f;
+		}
+
+		private void Update(){
+			var followPosition = followObject.transform.position + positionOffset;
+			var distance = Vector3.Distance(followPosition , transform.position);
+			rigidbody.velocity = (followPosition - transform.position).normalized * (followSpeed * distance);
+		}
+	}
+}
